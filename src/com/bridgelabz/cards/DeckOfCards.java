415c6dc823ba26d17d03e1a090084ac609bf55ee
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-21 14:10:29 +0530

    Refractor added distribution of cards to four players

--- src/com/bridgelabz/cards/DeckOfCards.java
+++ src/com/bridgelabz/cards/DeckOfCards.java
@@ -2,6 +2,57 @@ package com.bridgelabz.cards;
 
 public class DeckOfCards {
 
+	String player1[][] = new String[1][9];
+	String player2[][] = new String[1][9];
+	String player3[][] = new String[1][9];
+	String player4[][] = new String[1][9];
+
+	public void distributeCards(String[] deck) {
+
+		int j = 0, k = 0, l = 0, m = 0;
+		for (int i = 0; i < deck.length; i++) {
+			if (j < 9 || k < 9 || l < 9 || m < 9) {
+				if (i % 4 == 0) {
+					player1[0][j] = deck[i];
+					j++;
+				} else if (i % 4 == 1) {
+					player2[0][k] = deck[i];
+					k++;
+				} else if (i % 4 == 2) {
+					player3[0][l] = deck[i];
+					l++;
+				} else if (i % 4 == 3) {
+					player4[0][m] = deck[i];
+					m++;
+				}
+			}
+		}
+
+		System.out.println("player 1 cards : ");
+		for (int i = 0; i < player1[0].length; i++) {
+			System.out.print(player1[0][i] + " ");
+		}
+		System.out.println();
+
+		System.out.println("player 2 cards : ");
+		for (int i = 0; i < player2[0].length; i++) {
+			System.out.print(player2[0][i] + " ");
+		}
+		System.out.println();
+
+		System.out.println("player 3 cards : ");
+		for (int i = 0; i < player3[0].length; i++) {
+			System.out.print(player3[0][i] + " ");
+		}
+		System.out.println();
+
+		System.out.println("player 4 cards : ");
+		for (int i = 0; i < player4[0].length; i++) {
+			System.out.print(player4[0][i] + " ");
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
 		String[] deck = new String[52];
 		String[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
@@ -32,5 +83,7 @@ public class DeckOfCards {
 		for (int i = 0; i < deck.length; i++) {
 			System.out.println(deck[i]);
 		}
+		DeckOfCards deckOfCards = new DeckOfCards();
+		deckOfCards.distributeCards(deck);
 	}
 }
